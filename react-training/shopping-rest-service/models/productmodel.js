d3ec475c146d144165bdbbb8bf03c69d9e0fb686
coder-ankit <agarwalankit88@outlook.com>
2018-05-24 15:34:14 +0530

    shopping rest service

---
+++ react-training/shopping-rest-service/models/productmodel.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+
+var productSchema = mongoose.Schema({
+    name: String,
+    price: Number
+});
+
+module.exports = mongoose.model("cln_products", productSchema);
